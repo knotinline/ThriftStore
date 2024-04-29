e400226716caa850793498807281dcb51731f7d1
p14ntot <p14ntot@ionio.gr>
2024-04-29 18:58:54 +0300

    update searchbar component

--- ThriftStore/src/components/general-components/SearchBar.jsx
+++ ThriftStore/src/components/general-components/SearchBar.jsx
@@ -1,25 +1,28 @@
-import { BsSearchHeart } from "react-icons/bs";
 
-function SearchBar() {
+import { IoSearch } from "react-icons/io5";
 
-  return (
-    <form  className="flex items-center  mt-3 ml-1">
-      <input
-        type="text"
-        
-        placeholder="What do you fancy?..."
-        className="border border-gray-300 rounded-md px-4 py-2 mr-2 focus:outline-none focus:border-blue-500"/>
-        <button
-        type="submit"
-        
-      >
-        <BsSearchHeart />
-      </button>
+const SearchBar = () => {
+    return (
+        <div>
+            <form className="relative w-10/12 min-w-[140px] rounded-full h-auto  hover:border-transparent ">
+                <div className="relative ">
+                    <input
+                        type="text"
+                        placeholder="SEARCH HERE"
+                        className="w-full rounded-full p-4 h-6 focus:outline-none mt-5 ml-3 hover:shadow-lg focus:shadow-xl shadow-md"
+                    />
+                    <button
+                        type="submit"
+                        className="absolute right-1 top-1/2 rounded-full  p-1 -translate-y-1/2 h-6 mt-3"
+                    >
+                        <IoSearch />
+                    </button>
+                </div>
+            </form>
+        </div>
 
-      
-
-    </form>
-  );
-}
+    );
+};
 
 export default SearchBar;
+
