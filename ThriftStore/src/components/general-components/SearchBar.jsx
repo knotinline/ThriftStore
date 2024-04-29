7bb318cc5bf918d0d15189f8869e6e4602985cc7
knotinline <vassilikidimoka@gmail.com>
2024-04-29 17:31:55 +0200

    Added react icons and changed search bar icon

--- ThriftStore/src/components/general-components/SearchBar.jsx
+++ ThriftStore/src/components/general-components/SearchBar.jsx
@@ -1,20 +1,22 @@
+import { BsSearchHeart } from "react-icons/bs";
 
 function SearchBar() {
 
   return (
-    <form  className="flex items-center ">
+    <form  className="flex items-center  mt-3 ml-1">
       <input
         type="text"
         
         placeholder="What do you fancy?..."
-        className="border border-gray-300 rounded-md px-4 py-2 mr-2 focus:outline-none focus:border-blue-500"
-      />
-      <button
+        className="border border-gray-300 rounded-md px-4 py-2 mr-2 focus:outline-none focus:border-blue-500"/>
+        <button
         type="submit"
-        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        
       >
-        Search
+        <BsSearchHeart />
       </button>
+
+      
 
     </form>
   );
