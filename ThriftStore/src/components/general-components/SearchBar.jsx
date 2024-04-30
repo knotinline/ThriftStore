0d2b96c7a4519d1cbcff357548f762f1a6ee9610
p14ntot <p14ntot@ionio.gr>
2024-04-30 20:26:20 +0300

    tis klaneis?? toso trexoun

--- ThriftStore/src/components/general-components/SearchBar.jsx
+++ ThriftStore/src/components/general-components/SearchBar.jsx
@@ -1,20 +1,56 @@
+// import { IoSearch } from "react-icons/io5";
+
+// const SearchBar = () => {
+//     return (
+//         <div className="relative flex-grow max-w-xs">
+//             <form className="relative rounded-full hover:border-transparent">
+//                 <div className="flex items-center">
+//                     <input
+//                         type="text"
+//                         placeholder="What do you fancy?"
+//                         className="w-full rounded-full p-4 h-10 focus:outline-none hover:shadow-lg focus:shadow-xl shadow-md"
+//                     />
+//                     <button
+//                         type="submit"
+//                         className="rounded-full p-1 h-10"
+//                     >
+//                         <IoSearch />
+//                     </button>
+//                 </div>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default SearchBar;
+
+
 import { IoSearch } from "react-icons/io5";
+import { useState } from "react";
 
 const SearchBar = () => {
+    const [query, setQuery] = useState("");
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
+
     return (
         <div className="relative flex-grow max-w-xs">
             <form className="relative rounded-full hover:border-transparent">
-                <div className="flex items-center">
+                <div className="relative flex items-center">
                     <input
                         type="text"
                         placeholder="What do you fancy?"
-                        className="w-full rounded-full p-4 h-10 focus:outline-none hover:shadow-lg focus:shadow-xl shadow-md"
+                        className="w-full rounded-full p-4 h-10 focus:outline-none hover:shadow-lg focus:shadow-xl shadow-md pl-10 "
+                        value={query}
+                        onChange={handleInputChange}
                     />
                     <button
                         type="submit"
-                        className="rounded-full p-1 h-10"
+                        className="absolute inset-y-0 left-0 flex items-center justify-center rounded-l-full bg-white p-1 h-10"
                     >
-                        <IoSearch />
+                        <IoSearch className=" text-gray-500 mt-1 ml-2"/>
                     </button>
                 </div>
             </form>
@@ -23,5 +59,3 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
-
-
--- ThriftStore/src/components/top-navigation/TopNavigation.jsx
+++ ThriftStore/src/components/top-navigation/TopNavigation.jsx
@@ -5,15 +5,17 @@ import { BsEmojiWink } from "react-icons/bs";
 
 function TopNavigation() {
     return (
-        <div className="flex justify-between items-center mt-5 px-4 sm:px-8 lg:px-16">
-            <div className="flex items-center">
+        // <div className="flex justify-center items-center mt-5 px-4 sm:px-8 lg:px-16">
+        <div className="flex items-center ">
+            <div className="flex justify-center items-center mt-5 px-4 sm:px-8 lg:px-16  w-2/3">
                 <Logo />
                 <SearchBar />
-            </div>
-            <div className="flex items-center">
                 <button >
-                    <BsFilterLeft className="w-9 h-9 mr-7" />
+                    <BsFilterLeft className="w-7 h-7 mr-7 mt-2" />
                 </button>
+            </div>
+            <div className="flex w-1/3 justify-end mt-4 pr-7">
+
                 <button>
                     <BsEmojiWink className="w-9 h-9" />
                 </button>
