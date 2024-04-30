a8dede1c742920a8c2d6e98a1525963fbd189776
knotinline <vassilikidimoka@gmail.com>
2024-04-30 19:38:28 +0200

    Added a first draft of the user menu

--- ThriftStore/src/components/top-navigation/TopNavigation.jsx
+++ ThriftStore/src/components/top-navigation/TopNavigation.jsx
@@ -1,23 +1,23 @@
 import Logo from "../general-components/Logo";
 import SearchBar from "../general-components/SearchBar";
+import UserMenu from "./user-menu/UserMenu";
 import { BsFilterLeft } from "react-icons/bs";
 import { BsEmojiWink } from "react-icons/bs";
 
+
 function TopNavigation() {
     return (
-        // <div className="flex justify-center items-center mt-5 px-4 sm:px-8 lg:px-16">
-        <div className="flex items-center ">
-            <div className="flex justify-center items-center mt-5 px-4 sm:px-8 lg:px-16  w-2/3">
+        <div className="flex justify-between items-center mt-5 px-4 sm:px-8 lg:px-16">
+            <div className="flex items-center">
                 <Logo />
                 <SearchBar />
-                <button >
-                    <BsFilterLeft className="w-7 h-7 mr-7 mt-2" />
-                </button>
             </div>
-            <div className="flex w-1/3 justify-end mt-4 pr-7">
-
+            <div className="flex items-center">
+                <button >
+                    <BsFilterLeft className="w-9 h-9 mr-7" />
+                </button>
                 <button>
-                    <BsEmojiWink className="w-9 h-9" />
+                    <UserMenu/>
                 </button>
             </div>
         </div>
---
+++ ThriftStore/src/components/top-navigation/user-menu/UserMenu.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { BsEmojiWink } from "react-icons/bs";
+
+const UserMenu = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuOpen = () => {
+        setIsMenuOpen(true);
+    };
+
+    const handleMenuClose = () => {
+        setIsMenuOpen(false);
+    };
+
+    return (
+        <div className="relative inline-block">
+            <button
+                onMouseEnter={handleMenuOpen}
+                onMouseLeave={handleMenuClose}
+            >
+                <button>
+                    <BsEmojiWink className="w-9 h-9" />
+                </button>
+
+            </button>
+
+            {/* User menu */}
+            {isMenuOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-10">
+                    <div className="py-1">
+                        {/* Menu items */}
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+                            Log out
+                        </button>
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+                            Account Details
+                        </button>
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+                            Messages
+                        </button>
+                        {/* Add more menu items as needed */}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default UserMenu;
